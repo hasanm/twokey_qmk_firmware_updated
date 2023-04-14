42bbabd9774b5129f46215d1c9b2a0f63b66d60b
p-hasan <p-hasan@hasan-ubuntu.uleth.ca>
2023-04-14 16:25:47 -0600

    Some cleanups

--- keyboards/handwired/twokey/keymaps/default/keymap.c
+++ keyboards/handwired/twokey/keymaps/default/keymap.c
@@ -11,17 +11,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // LAYOUT_ortho_1x2(KC_MS_BTN2, MY_KEY)
   LAYOUT_ortho_1x2(KC_MS_BTN1, KC_MS_BTN2)
 };
-
-
-
-bool process_record_user (uint16_t keycode, keyrecord_t *record) {
-
-  switch(keycode) {
-  case KC_MY_KEY:
-      if (record->event.pressed) {
-          SEND_STRING("Test String");
-      }
-      return false;
-  }
-  return true; 
-}
